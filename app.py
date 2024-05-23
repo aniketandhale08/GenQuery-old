ef4c4f589971f8b22970d650e0f17fd5d8c88750
Aniket Andhale <99685171+aniketandhale08@users.noreply.github.com>
2024-05-23 16:41:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,55 @@ import os
 
 def configure():
     load_dotenv()
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel("gemini-pro")
+    return model
 
-GOOGLE_API_KEY = os.getenv(('GOOGLE_API_KEY'))
-genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("gemini-pro")
+def generate_sql_query(model, input_prompt):
+    template = """
+            Create a SQL query snippet using the below text:
+
+            ```
+            {text_input}
+            ```  
+            i just want a Sql query         
+    """
+    formatted_template = template.format(text_input=input_prompt)
+    response = model.generate_content(formatted_template)
+    sql_query = response.text.strip().lstrip("```sql").rstrip("```")
+    return sql_query
+
+def generate_expected_output(model, sql_query):
+    expected_output = """
+            What would be the expected response of this SQL query snippet:
+
+
+            ```
+            {sql_query}
+            ```  
+            Provide sample tabluer Response with No Explanation        
+    """
+    expected_output_formatted = expected_output.format(sql_query=sql_query)
+    response = model.generate_content(expected_output_formatted)
+    return response.text
+
+def generate_explanation(model, sql_query):
+    explanation = """
+            Explain this sql Query:
+
+
+            ```
+            {sql_query}
+            ```  
+            Please provide with simplest of explanation:      
+    """
+    explanation_formatted = explanation.format(sql_query=sql_query)
+    response = model.generate_content(explanation_formatted)
+    return response.text
 
 def main():
-    configure()
+    model = configure()
     st.set_page_config(page_title="GenQuery", page_icon="robat:")
 
     st.markdown(
@@ -32,43 +74,9 @@ def main():
 
     if submit:
         with st.spinner("Generating SQL Query.."):
-            template = """
-                    Create a SQL query snippet using the below text:
-
-                    ```
-                    {text_input}
-                    ```  
-                    i just want a Sql query         
-            """
-            formatted_template = template.format(text_input=text_input)
-            response = model.generate_content(formatted_template)
-            sql_query = response.text.strip().lstrip("```sql").rstrip("```")
-
-            expected_output = """
-                    What would be the expected response of this SQL query snippet:
-
-
-                    ```
-                    {sql_query}
-                    ```  
-                    Provide sample tabluer Response with No Explanation        
-            """
-            expected_output_formatted = expected_output.format(sql_query=sql_query)
-            eoutput = model.generate_content(expected_output_formatted)
-            eoutput = eoutput.text
-
-            explanation = """
-                    Explain this sql Query:
-
-
-                    ```
-                    {sql_query}
-                    ```  
-                    Please provide with simplest of explanation:      
-            """
-            explanation_formatted = explanation.format(sql_query=sql_query)
-            explanation = model.generate_content(explanation_formatted)
-            explanation = explanation.text
+            sql_query = generate_sql_query(model, text_input)
+            eoutput = generate_expected_output(model, sql_query)
+            explanation = generate_explanation(model, sql_query)
             with st.container():
                 st.success("Your SQL query has been successfully generated. Feel free to copy and paste it into your database management system to retrieve the requested records.")
                 st.code(sql_query, language="sql")
